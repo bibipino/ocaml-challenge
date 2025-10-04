c99a90e90e0f6c4b684a7fc9a19d9965f2e4937b
bibipino <bibyserra08@gmail.com>
2025-10-04 15:50:22 +0200

    Completed 1 Nand

---
+++ 1/nand/solve.ml
@@ -0,0 +1,28 @@
+let nand1 a b = not (a && b);;
+
+assert(nand1 false false = true);;
+assert(nand1 true true = false);;
+assert(nand1 true false = true);;
+assert(nand1 false true = true);;
+
+let nand2 a b = if a then
+    if b then 
+        false 
+    else 
+        true
+else true;;
+    
+
+assert(nand2 false false = true);;
+assert(nand2 true true = false);;
+assert(nand2 true false = true);;
+assert(nand2 false true = true);;
+
+let nand3 a b = match (a,b) with
+| (true, true) -> false
+| (_, _) -> true;;
+
+assert(nand3 false false = true);;
+assert(nand3 true true = false);;
+assert(nand3 true false = true);;
+assert(nand3 false true = true);;
