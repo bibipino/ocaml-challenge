2f49aa2376d4ffe9cdf202e7ea804daf9a15a359
bibipino <bibyserra08@gmail.com>
2025-10-04 16:03:14 +0200

    Completed 1 Xor

---
+++ 1/xor/solve.ml
@@ -0,0 +1,33 @@
+let xor1 a b = (a || b) && not (a && b);;
+
+assert(xor1 false false = false);;
+assert(xor1 true true = false);;
+assert(xor1 true false = true);;
+assert(xor1 false true = true);;
+
+let xor2 a b = if a then
+    if b then 
+        false 
+    else 
+        true
+else
+    if b then
+        true
+    else
+        false;;
+    
+
+assert(xor2 false false = false);;
+assert(xor2 true true = false);;
+assert(xor2 true false = true);;
+assert(xor2 false true = true);;
+
+let xor3 a b = match (a,b) with
+| (false, true) -> true
+| (true, false) -> true
+| (_, _) -> false;;
+
+assert(xor3 false false = false);;
+assert(xor3 true true = false);;
+assert(xor3 true false = true);;
+assert(xor3 false true = true);;
