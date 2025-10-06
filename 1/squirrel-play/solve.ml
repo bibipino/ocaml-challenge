9252ee111a2e1bd1588cb139fc0d26d6be50df8a
bibipino <bibyserra08@gmail.com>
2025-10-06 14:50:01 +0200

    Completed 1 Squirrel Play

---
+++ 1/squirrel-play/solve.ml
@@ -0,0 +1,10 @@
+type season = Spring | Summer | Autumn | Winter ;;
+
+let squirrel_play (t : int) (s : season) : bool = 
+  if (t <= 30 && t >= 15) || (t >= 15 && s == Summer && t <= 35) then true else false
+;;
+
+assert(squirrel_play 18 Winter = true);;
+assert(squirrel_play 32 Spring = false);;
+assert(squirrel_play 32 Summer = true);;
+assert(squirrel_play 11 Autumn = false);;
