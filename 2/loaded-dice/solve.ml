30d6e56a50b3aed795800141805f1ae5bf519bb5
bibipino <bibyserra08@gmail.com>
2025-10-07 15:04:28 +0200

    Completed 2 Loaded Dice 1.1

--- 2/loaded-dice/solve.ml
+++ 2/loaded-dice/solve.ml
@@ -4,5 +4,14 @@ let dice x = if x > 100 then failwith("simpaticone") else
     Random.int(5)+1
 ;;
 
+let test p tot =
+  let rec tr n p tot w =
+    if n = 0 then string_of_float ( 100. *. (float_of_int w /. float_of_int tot)) ^ "%" else
+      tr (n-1) p tot (w+ if dice p = 6 then 1 else 0)
+  in 
+  tr tot p tot 0;;
+
+test 80 1000000;;
+
 assert(dice 0 <> 6);;
 assert(dice 100 = 6);;
