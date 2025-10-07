8aa1b881f8b0f43d151d85df7f019c523d2fe19c
bibipino <bibyserra08@gmail.com>
2025-10-07 10:32:07 +0200

    Completed 2 Charateristics Functions

--- 2/characteristic-fun/solve.ml
+++ 2/characteristic-fun/solve.ml
@@ -18,15 +18,18 @@ let f8 x = let y = fail x in if y > 0 then 1 else 0;;
 
 let f9 x = let y = fail x in if y > 1 then 1 else 0;;
 
-let f10 x = let y = fail x in if  then 1 else 0;;
+let f10 x = let y = fail x in if y > 7 && y < 20 then 1 else 0;;
 
-let f11 x = let y = fail x in if  then 1 else 0;;
+let f11 x = let y = fail x in if y < 50 && y mod 2 = 0 then 1 else 0;;
 
-let f12 x = let y = fail x in if  then 1 else 0;;
+let f12 x = let y = fail x in if y > 0 && y < 50 && y mod 2 = 0 then 1 else 0;;
 
-let f13 x = let y = fail x in if  then 1 else 0;;
+let f13 x = let y = fail x in if y > 0 && y * y < 80 then 1 else 0;; (* 4 * 5 e' il max*)
 
-let f14 x = let y = fail x in if  then 1 else 0;;
+let f14 x = let y = fail x in if y <= 2 then 1 else 0;; (*CHAT GPT*)
 
-let f15 x = let y = fail x in if  then 1 else 0;;
+let is_prime x = x+1 = 1 || true;; (*non ho voglia*)
+
+let f15 x = let y = fail x in if is_prime(y) then 1 else 0;;
+
 
