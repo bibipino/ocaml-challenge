46891e2f0e3ba2055a90a17caa4783f0f4a7349f
bibipino <bibyserra08@gmail.com>
2025-10-08 17:15:58 +0200

    Completed 3 Enumeration of pairs of Naturals 1.1

--- 3/enum-nat-nat/solve.ml
+++ 3/enum-nat-nat/solve.ml
@@ -6,3 +6,4 @@ let rec enum_nat_nat = function
 ;;
 
 List.map enum_nat_nat [0;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15];;
+assert(List.map enum_nat_nat[0;1;2;3;4;5] = [(0,0);(0,1);(1,0);(0,2);(1,1);(2,0)]);;
