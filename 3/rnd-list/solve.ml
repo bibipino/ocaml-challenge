e9843b55fad70a4e988ab8561b1961cf61ac3b44
bibipino <bibyserra08@gmail.com>
2025-10-08 11:52:46 +0200

    Completed 3 Random List 1.1

--- 3/rnd-list/solve.ml
+++ 3/rnd-list/solve.ml
@@ -6,6 +6,14 @@ let rndLst n m =
     aux [] n m
 ;;
 
-let randoo = rndLst 100 20;;
+let rec rndLst2 n m =
+  if n = 0 then []
+  else (Random.int(m)+1) :: rndLst2 (n-1) m
+;;
 
-float_of_int (List.fold_right (+) randoo 0) /. (float_of_int (List.length randoo));;
+let rando1 = rndLst 100 20;;
+let rando2 = rndLst2 100 20;;
+
+
+float_of_int (List.fold_right (+) rando1 0) /. (float_of_int (List.length rando1));;
+float_of_int (List.fold_right (+) rando2 0) /. (float_of_int (List.length rando2));;
